65e3c3e4430c6d36bfe2a9f892c37d011ecb8638
Ken Lau <ken_lau177@hotmail.com>
2017-01-30 00:49:55 -0700

    First commit

---
+++ js/optimize.js
@@ -0,0 +1,160 @@
+// https://rosettacode.org/wiki/Combinations#JavaScript
+function combinations(arr, k){
+    var i,
+    subI,
+    ret = [],
+    sub,
+    next;
+    for(i = 0; i < arr.length; i++){
+        if(k === 1){
+            ret.push( [ arr[i] ] );
+        }else{
+            sub = combinations(arr.slice(i+1, arr.length), k-1);
+            for(subI = 0; subI < sub.length; subI++ ){
+                next = sub[subI];
+                next.unshift(arr[i]);
+                ret.push( next );
+            }
+        }
+    }
+    return ret;
+}
+
+function compute_expected_crib_hand_helper(fh, card, suit) {
+	var fh_combn = combinations(fh, 4);
+	var tot_score = 0;
+	var count = 0;
+	for (c in fh_combn) {
+		var card1 = fh_combn[c][0].split("-")[0];
+		var card2 = fh_combn[c][1].split("-")[0];
+		var card3 = fh_combn[c][2].split("-")[0];
+		var card4 = fh_combn[c][3].split("-")[0];
+
+		var suit1 = fh_combn[c][0].split("-")[1];
+		var suit2 = fh_combn[c][1].split("-")[1];
+		var suit3 = fh_combn[c][2].split("-")[1];
+		var suit4 = fh_combn[c][3].split("-")[1];		
+
+		tot_score += score_hand([card,card1,card2,card3,card4], [suit,suit1,suit2,suit3,suit4], 
+									suit4);
+		count++;
+	}
+  return tot_score/count;
+}
+
+function compute_expected_crib_hand(hand_names, hand_suits) {
+
+	full_hand_names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+	//full_hand_names = ['K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'A'];
+	//full_hand_names = ['4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+	full_hand_suits = ['S', 'H', 'C', 'D'];
+
+	var fh = [];
+	for (n in full_hand_names) {
+		for (s in full_hand_suits) {
+			fh.push(full_hand_names[n].concat('-').concat(full_hand_suits[s]));
+		}
+	}
+	
+	//var hand_names_suits = [];
+	//for (n in hand_names) {
+	//	hand_names_suits.push(hand_names[n].concat(hand_suits[n]));
+	//}
+
+	for (i=0; i<hand_names.length; i++) {
+		fh.splice(fh.indexOf(hand_names[i].concat('-').concat(hand_suits[i])), 1);		
+	}
+
+	var score1 = compute_expected_crib_hand_helper(fh, hand_names[0], hand_suits[0]);
+	var score2 = compute_expected_crib_hand_helper(fh, hand_names[1], hand_suits[1]);
+	var score3 = compute_expected_crib_hand_helper(fh, hand_names[2], hand_suits[2]);
+	var score4 = compute_expected_crib_hand_helper(fh, hand_names[3], hand_suits[3]);
+	var score5 = compute_expected_crib_hand_helper(fh, hand_names[4], hand_suits[4]);
+	
+	return [score1, score2, score3, score4, score5];
+}
+
+function compute_expected_hand_helper(fh, cards_keep, suits_keep) {
+	var tot_score = 0;
+	var count = 0;
+	for (f in fh) {
+
+		var card_cut = fh[f].split("-")[0];
+		var suit_cut = fh[f].split("-")[1];
+
+		tot_score += score_hand([card_cut, cards_keep[0], cards_keep[1],
+														cards_keep[2], cards_keep[3]],
+														[suit_cut, suits_keep[0], suits_keep[1],
+														suits_keep[2], suits_keep[3]], suit_cut);
+		count++;
+	}
+  return tot_score/count;
+}
+
+function compute_expected_hand(hand_names, hand_suits) {
+	full_hand_names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+	full_hand_suits = ['S', 'H', 'C', 'D'];
+
+	var fh = [];
+	for (n in full_hand_names) {
+		for (s in full_hand_suits) {
+			fh.push(full_hand_names[n].concat('-').concat(full_hand_suits[s]));
+		}
+	}
+	
+	for (i=0; i<hand_names.length; i++) {
+		fh.splice(fh.indexOf(hand_names[i].concat('-').concat(hand_suits[i])), 1);		
+	}
+
+	var score1 = compute_expected_hand_helper(fh, hand_names.slice(1), hand_suits.slice(1));
+	var score2 = compute_expected_hand_helper(fh, hand_names.slice(0,1).concat(hand_names.slice(2)), 
+																						hand_suits.slice(0,1).concat(hand_suits.slice(2)));
+	var score3 = compute_expected_hand_helper(fh, hand_names.slice(0,2).concat(hand_names.slice(3)), 
+																						hand_suits.slice(0,2).concat(hand_suits.slice(3)));
+	var score4 = compute_expected_hand_helper(fh, hand_names.slice(0,3).concat(hand_names.slice(4)), 
+																						hand_suits.slice(0,3).concat(hand_suits.slice(4)));
+	var score5 = compute_expected_hand_helper(fh, hand_names.slice(0,4), hand_suits.slice(0,4));
+
+	return [score1, score2, score3, score4, score5];
+}
+
+function findIndicesOfMax(inp, count) {
+  var outp = [];
+  for (var i = 0; i < inp.length; i++) {
+    outp.push(i); // add index to output array
+    if (outp.length > count) {
+      outp.sort(function(a, b) { return inp[b] - inp[a]; }); // descending sort the output array
+      outp.pop(); // remove the last index (index of smallest element in output array)
+    }
+  }
+  return outp;
+}
+
+function optimize(hand_names, hand_suits, yourcrib) {
+
+	var exp_scores_hand = compute_expected_hand(hand_names, hand_suits);
+	var exp_scores_crib = compute_expected_crib_hand(hand_names, hand_suits);
+
+	if (yourcrib) {
+		var exp_scores = [];
+		for (var i = 0; i < exp_scores_hand.length; i++){
+	  	exp_scores.push((exp_scores_hand[i] + exp_scores_crib[i]).toFixed(1));
+		}
+	} else {
+		var exp_scores = [];
+		for (var i = 0; i < exp_scores_hand.length; i++){
+	  	exp_scores.push((exp_scores_hand[i] - exp_scores_crib[i]).toFixed(1));
+		}
+	}
+
+	var tmp = findIndicesOfMax(exp_scores, 3);
+
+	return [[hand_names[tmp[0]].concat(hand_suits[tmp[0]]), exp_scores[tmp[0]], 
+					exp_scores_hand[tmp[0]].toFixed(1), exp_scores_crib[tmp[0]].toFixed(1)],
+					[hand_names[tmp[1]].concat(hand_suits[tmp[1]]), exp_scores[tmp[1]], 
+					exp_scores_hand[tmp[1]].toFixed(1), exp_scores_crib[tmp[1]].toFixed(1)],
+					[hand_names[tmp[2]].concat(hand_suits[tmp[2]]), exp_scores[tmp[2]], 
+					exp_scores_hand[tmp[2]].toFixed(1), exp_scores_crib[tmp[2]].toFixed(1)]];
+}
+
+
---
+++ js/score.js
@@ -0,0 +1,165 @@
+var set_matrix = [];
+set_matrix.push([[0], [1], [2], [3], [4]]);
+set_matrix.push([[0,1], [0,2], [0,3], [0,4], [1,2], [1,3], [1,4],
+								 [2,3], [2,4], [3,4]]);
+set_matrix.push([[0,1,2],[0,1,3],[0,2,3],[1,2,3],[0,1,4],[0,2,4],[1,2,4],[0,3,4],[1,3,4],[2,3,4]]);
+set_matrix.push([[0,1,2,3], [0,1,2,4], [0,1,3,4], [0,2,3,4], [1,2,3,4]]);
+set_matrix.push([[0,1,2,3,4]]);
+
+function convert_hand_to_nums(current_nums) {
+	var out = current_nums.slice();
+	for (i=0; i<out.length; i++) {
+		if (out[i] === 'A') { out[i] = 1; }
+		if (out[i] === 'J') { out[i] = 10; }
+		if (out[i] === 'Q') { out[i] = 10; }
+		if (out[i] === 'K') { out[i] = 10; }
+		out[i] = Number(out[i]);
+	}
+	return out;
+}
+
+function convert_hand_to_ordinal(current_nums) {
+	var out = current_nums.slice();
+	for (i=0; i<out.length; i++) {
+		if (out[i] === 'A') { out[i] = 1; }
+		if (out[i] === 'J') { out[i] = 11; }
+		if (out[i] === 'Q') { out[i] = 12; }
+		if (out[i] === 'K') { out[i] = 13; }
+		out[i] = Number(out[i]);
+	}
+	return out;	
+}
+
+function score_hand(hand_names, hand_suits, cut_card_suit) {
+	var hand = convert_hand_to_nums(hand_names);
+	var hand_ordinal = convert_hand_to_ordinal(hand_names);
+	var out = score_15s(hand) + score_pairs(hand_names) + score_flush_hand(hand_suits) + 
+							score_nobs(hand_names, hand_suits, cut_card_suit) + 
+							score_runs(hand_ordinal);
+	return out;
+}
+
+function score_15s(hand) {
+	var score = 0;
+	// Look through the '2' sets, then the 3, 4, and 5
+	for (set_index=1; set_index < 5; set_index++) {
+		for (set_length=0; set_length < set_matrix[set_index].length; set_length++) {
+			var sum = 0;
+			// Add up the cards pointed to by the indices of the set
+			for (set_member=0; set_member < set_matrix[set_index][set_length].length; set_member++) {
+				sum += hand[set_matrix[set_index][set_length][set_member]];
+			}
+			if (sum === 15) {
+				score += 2;
+			}
+		}
+	}
+	return score;
+}
+
+function score_pairs(hand) {
+	var score = 0;
+
+	// Look through the sets of indices in the "2" set
+	for (set_length = 0; set_length < set_matrix[1].length; set_length++) {
+		//Check the two cards pointed at by the indicies and see if they're the same
+		if (hand[set_matrix[1][set_length][0]] === hand[set_matrix[1][set_length][1]]) {
+			score += 2;
+		}
+	}
+	return score;
+}
+
+function score_flush_hand(hand_suits) {
+	var counted_suits = hand_suits.reduce(function(all_suits, suit) { 
+											  if (suit in all_suits) {
+											    all_suits[suit]++;
+											  }
+											  else {
+											    all_suits[suit] = 1;
+											  }
+											  return all_suits;
+											}, {});
+	counted_suits = Object.values(counted_suits);
+	if (counted_suits.indexOf(5) > -1) {
+		return 5;
+	} else if (counted_suits.indexOf(4) > -1) {
+		return 4;
+	} else {
+		return 0;
+	}
+}
+
+function score_flush_crib(hand_suits) {
+	var suit = hand_suits[0];
+
+	// Check the suit of each card in the hand against the suit of the first card.
+	// As soon as they don't match, skip out of the routine
+	for (i = 1; i < hand_suits.length; i++) {
+		if (hand_suits[i] != suit) {
+			return 0;
+		}
+	}
+
+	return hand_suits.length;
+}
+
+function score_nobs(hand_names, hand_suits, cut_card_suit) {
+	// Loop over all the cards in the hand, check to see if it's a Jack, and if it is, check the suit.
+	for (i=0; i<hand_names.length; i++) {
+		if (hand_names[i] === 'J' && hand_suits[i] === cut_card_suit) {
+			return 1;
+		}
+	}
+
+	// We haven't left the routine yet, so the right jack is not in the hand.
+	return 0;
+}
+
+function score_runs(hand) {
+	var hand_ordinal = hand.slice();
+	hand_ordinal.sort(function(a, b) { return a - b; });
+	var score = 0;
+	var run_found = false;
+
+	// Look for 5 card runs first, then 4 then 3.
+	for (i = 4; i > 1; i--) {
+		var sets = set_matrix[i];
+
+		// Iterate over each of the sets available for the length under examination.
+		for (set_index = 0; set_index < sets.length; set_index++) {
+			var local_run_found = true;
+
+			// Look at each index in the set, from the first to the second-to-last
+			for (set_member=0; set_member < sets[set_index].length-1; set_member++) {
+				first = hand_ordinal[sets[set_index][set_member]];
+				second = hand_ordinal[sets[set_index][set_member + 1]];
+
+				// Check to see if the second card is only 1 more than the current card.
+				// If it isn't, skip out of the loop. The two cards are not consecutive, so 
+				// we can't have a run of this length.
+				if (second - first !== 1) {
+					local_run_found = false;
+					break;
+				}
+			}
+			// If we're this far and the local_run_found flag has not been reset, 
+			// the current set defines a group of array indicies that are a run.
+			if (local_run_found) {
+				score += (i+1);
+				run_found = true;
+			}
+		}
+
+		// If a run has been found, don't look at the smaller runs
+		if (run_found) 
+			break;
+	}
+	
+	return score;
+}
+
+
+
+
+
